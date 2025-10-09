17ee24e540bd3839c05f77ff5faae3285b88f9e6
Nattachai Kaewchum <nattachaiguide@gmail.com>
2025-10-10 01:22:54 +0700

    pass to heart

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 const port = 3000;
 const sqlite3 = require("sqlite3").verbose();
 const session = require("express-session");
+const { table } = require("console");
+const { json } = require("stream/consumers");
 
 app.use(express.json());
 app.use(express.static("public"));
@@ -52,10 +54,9 @@ app.post("/api/detail", (req, res) => {
         res.json(rows);
     });
 });
-// name":"${item.value_name}", "price":${item.extra_price
+
 app.post("/api/get-cart", (req, res) => {
     const cart = req.session.cart || [];
-    console.log(cart);
     res.json(cart);
 });
 
@@ -64,7 +65,6 @@ app.get("/add-to-cart", (req, res) => {
     if (!req.session.cart) {
         req.session.cart = [];
     }
-    console.log(all);
     const options = [];
     for (const key in all) {
         if (!["menu_name", "menu_id", "totalPrice"].includes(key)) {
@@ -98,7 +98,70 @@ app.get("/menus", (req, res) => {
         .catch((err) => console.log(err));
 });
 
-app.get("/payment", (req, res) => {});
+app.get("/payment", (req, res) => {
+    res.render("payment");
+});
+
+app.get("/add-order/:table", (req, res) => {
+    const cart = req.session.cart || [];
+    const table = req.params.table;
+
+    const sql = `INSERT INTO orders (table_id, menu) VALUES (?, ?);`;
+
+    const menuData = JSON.stringify(cart);
+
+    db.run(sql, [table, menuData], (err) => {
+        if (err) {
+            console.error(err.message);
+            return res.redirect("/");
+        }
+        console.log(`Order placed for table ${table}`);
+        req.session.cart = [];
+        res.redirect("/");
+    });
+});
+
+app.get("/add-waiting-order/:table", (req, res) => {
+    const cart = req.session.cart || [];
+    const table = req.params.table;
+    let price = 0
+    cart.forEach(item => {price += parseInt(item.totalPrice)})
+    
+
+    const sql = `INSERT INTO waitingOrder (table_id, menu, price) VALUES (?, ?, ?);`;
+
+    const menuData = JSON.stringify(cart);
+
+    db.run(sql, [table, menuData, price], (err) => {
+        if (err) {
+            console.error(err.message);
+            return res.redirect("/");
+        }
+        console.log(`Waiting Order placed for table ${table}`);
+        req.session.cart = [];
+        res.redirect("/");
+    });
+});
+
+app.get("/emphome", (req, res) => {
+    res.render("emphome");
+});
+
+app.get("/confirm", (req, res) => {
+    const sql = `SELECT * FROM waitingOrder`
+    db.all(sql, (err, rows) => {
+        if (err) {
+            console.log(err.message)
+        } else {
+            console.log(rows)
+            res.render("confirm", {data: rows});
+        }
+    })
+});
+
+app.get("/barista", (req, res) => {
+    res.render("barista");
+});
 
 app.listen(port, (err) => {
     if (err) {
--- public/js/menus.js
+++ public/js/menus.js
@@ -3,17 +3,16 @@ function showDetail(id, name, price) {
     const form = document.getElementById(`customize-form-${id}`);
 
     if (!form.innerHTML) {
-        const endpoint = "http://localhost:3000/api/detail";
+        // const endpoint = "http://localhost:3000/api/detail";
+        const endpoint = `${window.location.origin}/api/detail`;
         const sendPackage = {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ menuID: id }),
         };
-
         fetch(endpoint, sendPackage)
             .then((response) => response.json())
             .then((options) => {
-                
                 const optionGroups = options.reduce((groups, item) => {
                     const key = item.option_id;
                     if (!groups[key]) {
@@ -29,16 +28,24 @@ function showDetail(id, name, price) {
                 let formHTML = `<input type="hidden" name="menu_id" value="${id}"><input type="hidden" name="menu_name" value="${name}"><input type="hidden" id="totalPrice" name="totalPrice" value="${price}">`;
                 for (const optionId in optionGroups) {
                     const group = optionGroups[optionId];
-                    const isCheckbox = group.values.some(
-                        (v) => [3,4].includes(v.option_id)
-                    ); 
+                    const isCheckbox = group.values.some((v) =>
+                        [3, 4].includes(v.option_id)
+                    );
                     const inputType = isCheckbox ? "checkbox" : "radio";
 
                     formHTML += `<div class="option-div"><h3>${group.name}</h3>`;
                     group.values.forEach((item) => {
                         formHTML += `
                             <div class='select-option'>
-                                <input ${[3,4].includes(item.option_id) ? "" : "required"} type="${inputType}" id="value-${item.value_id}" name="${item.option_id}" value='{"name":"${item.value_name}", "price":${item.extra_price}}'>
+                                <input ${
+                                    [3, 4].includes(item.option_id)
+                                        ? ""
+                                        : "required"
+                                } type="${inputType}" id="value-${
+                            item.value_id
+                        }" name="${item.option_id}" value='{"name":"${
+                            item.value_name
+                        }", "price":${item.extra_price}}'>
                                 <label for="value-${item.value_id}">
                                     <span>${item.value_name}</span>
                                     <span>+${item.extra_price} THB</span>
@@ -49,7 +56,7 @@ function showDetail(id, name, price) {
                 }
                 form.innerHTML = formHTML;
                 if (options.length === 0) {
-                    form.innerHTML += `<p>ไม่มีรายการให้ปรับแต่ง</p>`
+                    form.innerHTML += `<p>ไม่มีรายการให้ปรับแต่ง</p>`;
                 }
 
                 const formContainer = document.getElementById(
@@ -61,7 +68,7 @@ function showDetail(id, name, price) {
                     submitButton.setAttribute("form", `customize-form-${id}`);
                     submitButton.textContent = "เพิ่มลงตะกร้า";
                     submitButton.className = "add-to-cart-button";
-                    formContainer.appendChild(submitButton)
+                    formContainer.appendChild(submitButton);
                 }
 
                 form.addEventListener("change", () => {
@@ -84,7 +91,7 @@ function showDetail(id, name, price) {
                     });
 
                     priceContainer.innerText = `Price: ${totalPrice} THB`;
-                    newPrice.value = totalPrice
+                    newPrice.value = totalPrice;
                 });
             })
             .catch((err) => console.error("Error fetching details:", err));
@@ -96,61 +103,64 @@ const closeDetail = (id) => {
     const form = document.getElementById(`customize-form-${id}`);
     const detail = document.getElementById(`detail-container-${id}`);
     detail.style.display = "none";
-    form.innerHTML = ''
+    form.innerHTML = "";
 };
-
 
 // CART :DD
 function showCart() {
-    const cartIcon = document.getElementById('cart')
+    const cartIcon = document.getElementById("cart");
     const cart = document.getElementById("cart-container");
-    cart.innerHTML = ''
-
+    cart.innerHTML = "";
+    console.log(window.location.origin)
     if (!cart.innerHTML) {
-        cart.innerHTML += `<button class="close-cart-button" onclick="closeCart()">X</button><h1>ตะกร้าสินค้า</h1><div class="cart-item-container" id="cart-item-container"></div>`
-        const cartItemContainer = document.getElementById("cart-item-container")
-        const endpoint = "http://localhost:3000/api/get-cart";
+        cart.innerHTML += `<button class="close-cart-button" onclick="closeCart()">X</button><h1>ตะกร้าสินค้า</h1><div class="cart-item-container" id="cart-item-container"></div>`;
+        const cartItemContainer = document.getElementById(
+            "cart-item-container"
+        );
+        // const endpoint = "http://localhost:3000/api/get-cart";
+        const endpoint = `${window.location.origin}/api/get-cart`;
         const sendPackage = {
             method: "POST",
-            headers: { "Content-Type": "application/json" } 
+            headers: { "Content-Type": "application/json" },
         };
 
         fetch(endpoint, sendPackage)
             .then((response) => response.json())
             .then((items) => {
-                if (!(items.length === 0)){
-                    let finalPrice = 0
-                    items.forEach(item => {
-                        let itemCreate = ''
+                if (!(items.length === 0)) {
+                    let finalPrice = 0;
+                    items.forEach((item) => {
+                        let itemCreate = "";
                         itemCreate += `<div class="cart-item">
-                                        <p class="menu-name">รายการ : ${item.menu_name} x${1}</p>
-                                        <p style="margin-left: 7px;">รายละเอียดตัวเลือก : `
+                                        <p class="menu-name">รายการ : ${
+                                            item.menu_name
+                                        } x${1}</p>
+                                        <p style="margin-left: 7px;">รายละเอียดตัวเลือก : `;
                         if (!(item.options.length === 0)) {
-                            item.options.forEach(value => {
-                                itemCreate += `${value.name} `
-                            })
+                            item.options.forEach((value) => {
+                                itemCreate += `${value.name} `;
+                            });
                         } else {
-                            itemCreate += `-`
+                            itemCreate += `-`;
                         }
-                        itemCreate += `</p><p>ราคา : ${item.totalPrice} บาท</p></div>`
-                        cartItemContainer.innerHTML += itemCreate
-                        finalPrice += parseInt(item.totalPrice)
-                    })
+                        itemCreate += `</p><p>ราคา : ${item.totalPrice} บาท</p></div>`;
+                        cartItemContainer.innerHTML += itemCreate;
+                        finalPrice += parseInt(item.totalPrice);
+                    });
                     const html = `<div class="total-price-container">
                         <p>ราคารวม : ${finalPrice} บาท</p>
                         <a href="/payment"><div class="payment-button">ชำระเงิน</div></a>
-                    </div>`
-                    cart.innerHTML += html
+                    </div>`;
+                    cart.innerHTML += html;
                 } else {
-                    let itemCreate = `<p>ไม่มีรายการสินค้า</p>`
-                    cartItemContainer.innerHTML += itemCreate
+                    let itemCreate = `<p>ไม่มีรายการสินค้า</p>`;
+                    cartItemContainer.innerHTML += itemCreate;
                 }
-                
             })
             .catch((err) => console.error("Error fetching details:", err));
-    } 
+    }
     cart.style.display = "flex";
-};
+}
 
 const closeCart = () => {
     const cart = document.getElementById("cart-container");
